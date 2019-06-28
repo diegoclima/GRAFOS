dbff74658ddf84f64d244c71531f3007e955ffdc
DIEGO CRISTOPHER <diegomartins1609@gmail.com>
2019-06-28 00:39:25 -0300

    Criação da classe grafo

---
+++ Grafos/Grafos/Classes/Grafo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grafos.Classes.System;
+namespace Grafos.Classes.System {
+    public class Grafo {
+        protected List<Aresta> vertices = new List<Aresta>();
+        protected int totalVerticesGrafo = 0;
+
+        public Grafo() { }
+
+        public override string ToString() {
+            string retorno = " ";
+            foreach(Aresta a in this.vertices) {
+                retorno += "| " + a.getVerticeInicial().getNome() + "-> " + (a.getVerticeFinal() == null ? " " : a.getVerticeFinal().getNome())
+            }
+            return retorno;
+        }
+
+        public virtual string setReposta(string resp) {
+            return resp;
+        }
+    }
+}
